804ed8d3dfe6e9dd342dbcc02fc48d9d826094d7
Torsten Scholak <torsten.scholak@googlemail.com>
2020-07-02 18:14:02 -0400

    apply black auto formatting

--- preprocess/parse_raw_json.py
+++ preprocess/parse_raw_json.py
@@ -6,17 +6,18 @@ import argparse
 from process_sql import get_sql
 
 
-#TODO: update the following dirs
-sql_path = 'spider/train.json'
-db_dir = 'database/'
-output_file = 'dev_new.json'
-table_file = 'spider/tables.json'
+# TODO: update the following dirs
+sql_path = "spider/train.json"
+db_dir = "database/"
+output_file = "dev_new.json"
+table_file = "spider/tables.json"
 
 
 class Schema:
     """
     Simple schema which maps table&column to a unique identifier
     """
+
     def __init__(self, schema, table):
         self._schema = schema
         self._table = table
@@ -31,13 +32,13 @@ class Schema:
         return self._idMap
 
     def _map(self, schema, table):
-        column_names_original = table['column_names_original']
-        table_names_original = table['table_names_original']
-        #print 'column_names_original: ', column_names_original
-        #print 'table_names_original: ', table_names_original
+        column_names_original = table["column_names_original"]
+        table_names_original = table["table_names_original"]
+        # print 'column_names_original: ', column_names_original
+        # print 'table_names_original: ', table_names_original
         for i, (tab_id, col) in enumerate(column_names_original):
             if tab_id == -1:
-                idMap = {'*': i}
+                idMap = {"*": i}
             else:
                 key = table_names_original[tab_id].lower()
                 val = col.lower()
@@ -48,21 +49,24 @@ class Schema:
             idMap[key] = i
 
         return idMap
-    
+
 
 def get_schemas_from_json(fpath):
     with open(fpath) as f:
         data = json.load(f)
-    db_names = [db['db_id'] for db in data]
+    db_names = [db["db_id"] for db in data]
 
     tables = {}
     schemas = {}
     for db in data:
-        db_id = db['db_id']
-        schema = {} #{'table': [col.lower, ..., ]} * -> __all__
-        column_names_original = db['column_names_original']
-        table_names_original = db['table_names_original']
-        tables[db_id] = {'column_names_original': column_names_original, 'table_names_original': table_names_original}
+        db_id = db["db_id"]
+        schema = {}  # {'table': [col.lower, ..., ]} * -> __all__
+        column_names_original = db["column_names_original"]
+        table_names_original = db["table_names_original"]
+        tables[db_id] = {
+            "column_names_original": column_names_original,
+            "table_names_original": table_names_original,
+        }
         for i, tabn in enumerate(table_names_original):
             table = str(tabn.lower())
             cols = [str(col.lower()) for td, col in column_names_original if td == i]
@@ -70,7 +74,6 @@ def get_schemas_from_json(fpath):
         schemas[db_id] = schema
 
     return schemas, db_names, tables
-
 
 
 schemas, db_names, tables = get_schemas_from_json(table_file)
@@ -92,6 +95,6 @@ for data in sql_data:
     except:
         print("db_id: ", db_id)
         print("sql: ", sql)
-        
-with open(output_file, 'wt') as out:
-    json.dump(sql_data_new, out, sort_keys=True, indent=4, separators=(',', ': '))
+
+with open(output_file, "wt") as out:
+    json.dump(sql_data_new, out, sort_keys=True, indent=4, separators=(",", ": "))
--- preprocess/parse_sql_one.py
+++ preprocess/parse_sql_one.py
@@ -13,10 +13,12 @@ from os.path import isfile, isdir, join, split, exists, splitext
 
 from process_sql import get_sql
 
+
 class Schema:
     """
     Simple schema which maps table&column to a unique identifier
     """
+
     def __init__(self, schema, table):
         self._schema = schema
         self._table = table
@@ -31,13 +33,13 @@ class Schema:
         return self._idMap
 
     def _map(self, schema, table):
-        column_names_original = table['column_names_original']
-        table_names_original = table['table_names_original']
-        #print 'column_names_original: ', column_names_original
-        #print 'table_names_original: ', table_names_original
+        column_names_original = table["column_names_original"]
+        table_names_original = table["table_names_original"]
+        # print 'column_names_original: ', column_names_original
+        # print 'table_names_original: ', table_names_original
         for i, (tab_id, col) in enumerate(column_names_original):
             if tab_id == -1:
-                idMap = {'*': i}
+                idMap = {"*": i}
             else:
                 key = table_names_original[tab_id].lower()
                 val = col.lower()
@@ -53,16 +55,19 @@ class Schema:
 def get_schemas_from_json(fpath):
     with open(fpath) as f:
         data = json.load(f)
-    db_names = [db['db_id'] for db in data]
+    db_names = [db["db_id"] for db in data]
 
     tables = {}
     schemas = {}
     for db in data:
-        db_id = db['db_id']
-        schema = {} #{'table': [col.lower, ..., ]} * -> __all__
-        column_names_original = db['column_names_original']
-        table_names_original = db['table_names_original']
-        tables[db_id] = {'column_names_original': column_names_original, 'table_names_original': table_names_original}
+        db_id = db["db_id"]
+        schema = {}  # {'table': [col.lower, ..., ]} * -> __all__
+        column_names_original = db["column_names_original"]
+        table_names_original = db["table_names_original"]
+        tables[db_id] = {
+            "column_names_original": column_names_original,
+            "table_names_original": table_names_original,
+        }
         for i, tabn in enumerate(table_names_original):
             table = str(tabn.lower())
             cols = [str(col.lower()) for td, col in column_names_original if td == i]
@@ -72,17 +77,14 @@ def get_schemas_from_json(fpath):
     return schemas, db_names, tables
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
+
     sql = "SELECT name ,  country ,  age FROM singer ORDER BY age DESC"
     db_id = "concert_singer"
     table_file = "tables.json"
-    
+
     schemas, db_names, tables = get_schemas_from_json(table_file)
     schema = schemas[db_id]
     table = tables[db_id]
     schema = Schema(schema, table)
     sql_label = get_sql(schema, sql)
-
-
-
